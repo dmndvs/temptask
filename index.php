e3cc5a7b5e525b99e42511a8152386728850073a
dmndvs <domin.dvs@gmail.com>
2015-10-14 15:50:05 +0300

    index file

---
+++ index.php
@@ -0,0 +1,27 @@
+<link href="css/styles.css" rel="stylesheet" type="text/css" />
+<?php
+define('PROT',1);
+include('lib/module.php');
+if(isset($_POST['oklogin'])) //when press button for authorization 
+	{
+		$error = enter(); //enter on site
+		if (!$error) //if no errors -> authorization
+		{
+			if(isset($_SESSION['id'])) {$action = 'congratulation';
+			}
+		}
+	}
+elseif(isset($_POST['okreg']))//when press button for registration 
+	{
+	$error = adduser();
+	if($error == 'user already exist') {$action='signup';} //when user is already exist, we still standing on "signup" form
+	}
+if(!$action and !$_GET['action']) {
+		$action = 'login';
+		}
+elseif(!$action and isset($_GET['action']))	{
+		$action=$_GET['action'];
+		}
+include ('include/'.$action.'.html'); //file with form
+session_destroy();
+?>
